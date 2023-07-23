8c062a946296ff3ad1db3d8a232f8c67589c6262
Jiaxun Yang <kevin.jx.yang@gmail.com>
2023-07-23 03:51:08 -0400

    Add GamePrepInteractorTest.java

--- src/test/java/use_cases/game_preparation_use_case/GamePrepInteractorTest.java
+++ src/test/java/use_cases/game_preparation_use_case/GamePrepInteractorTest.java
@@ -1,4 +1,21 @@
+package use_cases.game_preparation_use_case;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GamePrepInteractorTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void promptPlayerInfo() {
+    }
+
+    @Test
+    void processPlayerInfo() {
+    }
 }
