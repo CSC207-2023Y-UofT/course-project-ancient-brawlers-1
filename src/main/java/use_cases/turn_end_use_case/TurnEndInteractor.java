a621647cd762523ea728170c66bd9692aefff929
Your Name <you@example.com>
2023-07-24 19:55:58 -0400

    turn end use case

---
+++ src/main/java/use_cases/turn_end_use_case/TurnEndInteractor.java
@@ -0,0 +1,111 @@
+package use_cases.turn_end_use_case;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import entities.GameState;
+import entities.Player;
+import entities.cardEffects.CardEffect;
+import entities.cardEffects.CreatureStatsEffect;
+import entities.cardEffects.PlayerStatsEffect;
+import entities.cards.Card;
+import entities.cards.CreatureCard;
+import entities.cards.StructureCard;
+
+public class TurnEndInteractor implements TurnEndInputBoundary {
+
+    private final GameState gameState;
+    private final TurnEndOutputBoundary turnEndPresenter;
+
+    public TurnEndInteractor(GameState gameState, TurnEndOutputBoundary turnEndPresenter) {
+        this.gameState = gameState;
+        this.turnEndPresenter = turnEndPresenter;
+    }
+    
+    @Override
+    public void passTurn() {
+        gameState.switchPlayer();
+        // 1. pass player's name to presenter
+        // 2. whatever other effects that are needed
+    }
+
+    @Override
+    public TurnEndResponseModel triggerEndTurnEffects() {
+        Player currentPlayer = gameState.getCurrentPlayer();
+        Player nextPlayer = gameState.getOpposingPlayer();
+
+
+        // // 1. Check if the current player has a structure in play
+        // // 1a. Yes, lets trigger the event
+        if (currentPlayer.getStructure() != null) {
+            // Trigger the effects needed
+            StructureCard currentStructure = currentPlayer.getStructure();
+            List<CreatureCard> allTargets = new ArrayList<>();
+
+            switch (currentStructure.getTargetType()) {
+                // on the player themselves
+                case SELF:
+                    for (CardEffect effect : currentStructure.getEffects()) {
+                        if (effect instanceof PlayerStatsEffect) {
+                            ((PlayerStatsEffect) effect).invokeEffect(currentPlayer);
+                        }
+                    }
+                    break;
+                case ENEMY:
+                    allTargets.addAll(nextPlayer.getCreatures());
+                    break;
+                case ALL:
+                    allTargets.addAll(nextPlayer.getCreatures());
+                    allTargets.addAll(currentPlayer.getCreatures());
+                    break;
+                case FRIENDLY:
+                    allTargets.addAll(currentPlayer.getCreatures());
+                    break;
+                default:
+                    break;
+            }
+
+            for (CreatureCard creature : allTargets) {
+                for (CardEffect effect : currentStructure.getEffects()) {
+                    if (effect instanceof CreatureStatsEffect) {
+                        ((CreatureStatsEffect) effect).invokeEffect(creature);
+                    }
+                }
+            }
+        }
+
+        List<Integer> creatureIds1 = new ArrayList<>();
+        List<Integer> creatureIds2 = new ArrayList<>();
+        List<Integer> hitPoints1 = new ArrayList<>();
+        List<Integer> hitPoints2 = new ArrayList<>();
+        List<Integer> attack1 = new ArrayList<>();
+        List<Integer> attack2 = new ArrayList<>();
+        List<Integer> playerHandIds1 = new ArrayList<>();
+        List<Integer> playerHandIds2 = new ArrayList<>();
+        
+        for (CreatureCard creature : currentPlayer.getCreatures()) {
+            creature.clearStun();
+            attack1.add(creature.getTotalAttackDamage());
+            creatureIds1.add(creature.getId());
+            hitPoints1.add(creature.getTotalHitPoints());
+        }
+
+        for (Card currentCard : currentPlayer.getHand()) {
+            playerHandIds1.add(currentCard.getId());
+        }
+
+        for (CreatureCard creature : nextPlayer.getCreatures()) {
+            attack2.add(creature.getTotalAttackDamage());
+            creatureIds2.add(creature.getId());
+            hitPoints2.add(creature.getTotalHitPoints());
+        }
+
+        for (Card currentCard : nextPlayer.getHand()) {
+            playerHandIds2.add(currentCard.getId());
+        }
+
+        TurnEndResponseModel turnEndModel = new TurnEndResponseModel(hitPoints1, hitPoints2, attack1, attack2, creatureIds1, creatureIds2, playerHandIds1, playerHandIds2);
+        
+        return turnEndPresenter.showTurnEndScreen(turnEndModel);
+    }
+}
---
+++ src/main/java/use_cases/turn_end_use_case/TurnEndResponseModel.java
@@ -0,0 +1,89 @@
+package use_cases.turn_end_use_case;
+
+import java.util.List;
+
+public class TurnEndResponseModel {
+    private List<Integer> hitPoints1;
+    private List<Integer> hitPoints2;
+    private List<Integer> attack1;
+    private List<Integer> attack2;
+    private List<Integer> creatureIds1;
+    private List<Integer> creatureIds2;
+    private List<Integer> playerHandIds1;
+    private List<Integer> playerHandIds2;
+
+    public TurnEndResponseModel(List<Integer> hitPoints1, List<Integer> hitPoints2, List<Integer> attack1, List<Integer> attack2, List<Integer> creatureIds1, List<Integer> creatureIds2, List<Integer> playerHandIds1, List<Integer> playerHandIds2) {
+        this.hitPoints1 = hitPoints1;
+        this.hitPoints2 = hitPoints2;
+        this.attack1 = attack1;
+        this.attack2 = attack2;
+        this.creatureIds1 = creatureIds1;
+        this.creatureIds2 = creatureIds2;
+        this.playerHandIds1 = playerHandIds1;
+        this.playerHandIds2 = playerHandIds2;
+    }
+
+    public List<Integer> getCreatureIds1() {
+        return this.creatureIds1;
+    }
+
+    public List<Integer> getCreatureIds2() {
+        return this.creatureIds2;
+    }
+
+    public List<Integer> getAttack1() {
+        return this.attack1;
+    }
+
+    public List<Integer> getAttack2() {
+        return this.attack2;
+    }
+
+    public List<Integer> getHitPoints1() {
+        return this.hitPoints1;
+    }
+
+    public List<Integer> getHitPoints2() {
+        return this.hitPoints2;
+    }
+
+    public List<Integer> getPlayerHandIds1() {
+        return this.playerHandIds1;
+    }
+
+    public List<Integer> getPlayerHandIds2() {
+        return this.playerHandIds2;
+    }
+
+    public void setAttack1(List<Integer> attack1) {
+        this.attack1 = attack1;
+    }
+
+    public void setAttack2(List<Integer> attack2) {
+        this.attack2 = attack2;
+    }
+
+    public void setHitPoints1(List<Integer> hitPoints1) {
+        this.hitPoints1 = hitPoints1;
+    }
+
+    public void setHitPoints2(List<Integer> hitPoints2) {
+        this.hitPoints2 = hitPoints2;
+    }
+
+    public void setCreatureIds1(List<Integer> creatureIds1) {
+        this.creatureIds1 = creatureIds1;
+    }
+
+    public void setCreatureIds2(List<Integer> creatureIds2) {
+        this.creatureIds2 = creatureIds2;
+    }
+
+    public void setPlayerHands1(List<Integer> playerHandIds1) {
+        this.playerHandIds1 = playerHandIds1;
+    }
+
+    public void setPlayerHands2(List<Integer> playerHandIds2) {
+        this.playerHandIds2 = playerHandIds2;
+    }
+}
