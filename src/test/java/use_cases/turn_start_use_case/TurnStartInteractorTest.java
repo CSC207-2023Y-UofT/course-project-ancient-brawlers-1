4ceccf60d9e7d6b571586187f373a51a4569a8dc
pranavsethi22 <84486681+pranavsethi22@users.noreply.github.com>
2023-07-29 16:47:28 -0400

    added edge case tests to turn start use case

--- src/test/java/use_cases/turn_start_use_case/TurnStartInteractorTest.java
+++ src/test/java/use_cases/turn_start_use_case/TurnStartInteractorTest.java
@@ -255,5 +255,111 @@ class TurnStartInteractorTest {
         interactor.triggerTurnStartEffects();
     }
 
+    @Test
+    void testDrawCards_ExceedHandCapacityAfterTwoDraws() {
+        CardFactory cardFactory = new CardFactory();
+        PlayerFactory playerFactory = new PlayerFactory();
+        DeckFactory deckFactory = new DeckFactory();
+
+        List<CardEffect> effects = new ArrayList<>(List.of(new HealEffect(10)));
+        PlayableCardData cardData = new PlayableCardData("Some card", TargetType.ANY, effects);
+        Card card1 = cardFactory.createActionCard("name1", cardData);
+
+        Deck playerDeck1 = deckFactory.createPlayerDeck(new ArrayList<>(List.of(card1, card1, card1)));
+        Deck essenceDeck1 = deckFactory.createEssenceDeck(cardFactory);
+        Deck playerDeck2 = deckFactory.createPlayerDeck(new ArrayList<>(List.of(card1, card1, card1)));
+        Deck essenceDeck2 = deckFactory.createEssenceDeck(cardFactory);
+
+
+
+        CreatureCard creature1 = (CreatureCard) cardFactory.createCreatureCard("Name1", 1, 1, 1, 1);
+        CreatureCard creature2 = (CreatureCard) cardFactory.createCreatureCard("Name2", 1, 1, 1, 1);
+
+        Player player1 = playerFactory.createPlayer("Player 1", new ArrayList<>(List.of(creature1)),
+                (PlayerDeck) playerDeck1, (EssenceDeck) essenceDeck1);
+        Player player2 = playerFactory.createPlayer("Player 2", new ArrayList<>(List.of(creature2)),
+                (PlayerDeck) playerDeck2, (EssenceDeck) essenceDeck2);
+
+        // Leave Player 1 with an empty hand, but give Player 2 a hand of 7 cards.
+        for (int i = 0; i < 8; i++) {
+            player2.addCard(player2.getEssenceDeck().draw());
+        }
+        gameState = new GameState();
+        gameState.setPlayers(player1, player2);
+
+        TurnStartOutputBoundary presenter = new TurnStartOutputBoundary() {
+            @Override
+            public DrawCardOutputModel showDrawResult(DrawCardOutputModel outputData) {
+                assertEquals(2, outputData.getNewCardIds().size(),
+                        "2 play deck card should be drawn.");
+                assertEquals(1, outputData.getNewCardIds().size(),
+                        "The card drawn should have id 1, which comes from the PlayerDeck, not EssenceDeck.");
+                assertTrue(outputData.getNewCardNames().contains("name1"),
+                        "The output should contain the name of the card.");
+                assertEquals(2, outputData.getBurntCardIds().size(),
+                        "2 cards should be discarded because hand is full.");
+                assertTrue(outputData.getBurntCardNames().contains("Essence"),
+                        "There should be cards with name 'Essence' in the discarded cards.");
+                assertFalse(outputData.getBurntCardNames().contains("Name 1"),
+                        "There should be no cards with name 'Name 1' in the discarded cards.");
+                return null;
+            }
+
+            @Override
+            public CreatureStatsUpdateModel showClearBuffs(CreatureStatsUpdateModel outputData) {
+                fail("drawCards() should not call showClearBuffs().");
+                return null;
+            }
+
+            @Override
+            public TriggerEffectUpdateModel showEffectUpdates(TriggerEffectUpdateModel outputData) {
+                fail("drawCards() should not call showEffectUpdates().");
+                return null;
+            }
+        };
+        gameState.switchPlayer();  // this test is for Player 2.
+        TurnStartInteractor interactor = new TurnStartInteractor(gameState, presenter);
+        interactor.drawCards();
+
+    }
+
+    @Test
+    void testTriggerTurnStartEffects_onePlayerCardEffect() {
+        List<CardEffect> effects = new ArrayList<>(List.of(new DamageEffect(10)));
+        PlayableCardData cardData = new PlayableCardData("desc", TargetType.ENEMY, effects);
+        // this one triggers at TURN_END, so it should not be effective here.
+        StructureCard structure = new StructureCard(100, "struct", cardData, GameEvent.TURN_END);
+        gameState.getCurrentPlayer().setStructure(structure);
+
+        TurnStartOutputBoundary presenter = new TurnStartOutputBoundary() {
+            @Override
+            public DrawCardOutputModel showDrawResult(DrawCardOutputModel outputData) {
+                fail("triggerTurnStartEffects() should not call showDrawResult().");
+                return null;
+            }
+
+            @Override
+            public CreatureStatsUpdateModel showClearBuffs(CreatureStatsUpdateModel outputData) {
+                fail("triggerTurnStartEffects() should not call showClearBuffs().");
+                return null;
+            }
+
+            @Override
+            public TriggerEffectUpdateModel showEffectUpdates(TriggerEffectUpdateModel outputData) {
+                CreatureStatsUpdateModel creatureStats = outputData.getAllCreatureStats();
+                assertEquals(List.of(1), creatureStats.getHitPoints1());
+                assertEquals(List.of(1), creatureStats.getHitPoints2());
+                return null;
+            }
+        };
+        TurnStartInteractor interactor = new TurnStartInteractor(gameState, presenter);
+        interactor.triggerTurnStartEffects();
+    }
+
 }
+
+
+
+
+
 
