2d7570510eab3e5268f5dd2d38c136d43a403f71
Kaifeng-Li <kaifeng.li@mail.utoronto.ca>
2023-08-06 01:16:48 -0400

    WinLossPresenter and VictoryScreenModel finished

---
+++ src/main/java/interface_adapters/presenters/WinLossPresenter.java
@@ -0,0 +1,32 @@
+package interface_adapters.presenters;
+
+import game_ui.VictoryScreen;
+import interface_adapters.view_models.GameFrameModel;
+import interface_adapters.view_models.GameScreenType;
+import interface_adapters.view_models.GameplayScreenModel;
+import interface_adapters.view_models.VictoryScreenModel;
+import use_cases.win_loss_use_case.WinLossOutputBoundary;
+import use_cases.win_loss_use_case.WinLossResponseModel;
+
+import java.util.Objects;
+
+public class WinLossPresenter implements WinLossOutputBoundary {
+
+    private final GameFrameModel frameModel;
+    private final VictoryScreenModel victoryScreenModel;
+
+    public WinLossPresenter(GameFrameModel frameModel, VictoryScreenModel victoryScreenModel) {
+        this.frameModel = frameModel;
+        this.victoryScreenModel = victoryScreenModel;
+    }
+
+    @Override
+    public WinLossResponseModel showVictoryScreen(WinLossResponseModel outputData) {
+        if(!outputData.getWinnerName().equals("")){
+            victoryScreenModel.setWinnerName(outputData.getWinnerName());
+            frameModel.setCurrentScreen(GameScreenType.VICTORY);
+            return outputData;
+        }
+        return outputData;
+    }
+}
